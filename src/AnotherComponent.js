acbc9991fda0d23f582f7c71544916eb7578a5d2
Mageshwaran-Baskar <55185936+Mageshwaran-Baskar@users.noreply.github.com>
2020-02-20 12:28:42 +0530

    added components

---
+++ src/AnotherComponent.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+class  AnotherComponent extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { isEmptyState: true }
+ 
+    }
+  render() {
+  return (
+    <div >
+      
+        <h1 style={{color:"blue"}}>Hey Paul</h1>
+        <p>
+        <span style={{color:"rgb(184, 202, 22)"}}> I KNOW YOU WILL CLICK THIS BUTTON </span> 
+        <span style={{color:"red"}}>(Its human tendency)</span> </p>
+        
+       
+    </div>
+  );
+}
+}
+
+export default AnotherComponent;
